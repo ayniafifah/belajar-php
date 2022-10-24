64fe87e17ddd8f6b470e4612e98d6c6cf5603bf9
Ayni Afifah <ayniafifah86@gmail.com>
2022-10-24 11:14:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Ayni Afifah";
+$umur = 20;
+
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.";
+
+$namaKakak = "Sahrul";
+$umurKakak = 26;
+
+echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun.<br>";
+
+$selisihUmur = $umur - $umurKakak;
+
+echo "Selisih usia saya dan kakak saya adalah $selisihUmur tahun.";
+
+
+
+
+
+?>
